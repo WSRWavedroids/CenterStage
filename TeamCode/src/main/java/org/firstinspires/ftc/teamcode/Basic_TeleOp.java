16eb0b8fe0be292bffd021309ad5d8c22be888fe
WSRWaveDroids <wavedroid13206@gmail.com>
2023-09-07 22:40:08 -0500

    Adding another computer to the party

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic_TeleOp.java
@@ -30,8 +30,8 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import java.lang.Math;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -67,7 +67,6 @@ public class Basic_TeleOp extends OpMode {
     /*
      * Code to run ONCE when the driver hits INIT
      */
-    @Override
     public void init() {
 
         // Call the initialization protocol from the Robot class.
@@ -80,13 +79,11 @@ public class Basic_TeleOp extends OpMode {
     /*
      * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
      */
-    @Override
     public void init_loop() {telemetry.addData("HYPE", "ARE! YOU! READY?!?!?!?!");}
 
     /*
      * Code to run ONCE when the driver hits PLAY
      */
-    @Override
     public void start() {
         runtime.reset();
         telemetry.addData("HYPE", "Let's do this!!!");
@@ -95,7 +92,6 @@ public class Basic_TeleOp extends OpMode {
     /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
-    @Override
     public void loop() {
 
         singleJoystickDrive();
@@ -169,7 +165,6 @@ public class Basic_TeleOp extends OpMode {
     /*
      * Code to run ONCE after the driver hits STOP
      */
-    @Override
     public void stop () { telemetry.addData("Status", "Robot Stopped"); }
 
 
