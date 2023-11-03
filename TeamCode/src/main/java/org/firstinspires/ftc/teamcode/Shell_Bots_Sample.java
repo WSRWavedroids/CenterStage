ae46d3f00ecd5384964f5db5d43f25580f31f991
WSRWavedroids <wavedroid13206@gmail.com>
2023-11-02 22:59:46 -0500

    GP!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shell_Bots_Sample.java
@@ -0,0 +1,210 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
+/*
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Basic: Iterative OpMode", group="Iterative OpMode")
+@Disabled
+public class Shell_Bots_Sample extends OpMode
+{
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftBackMotor = null;
+    private DcMotor rightBackMotor = null;
+    private DcMotor leftFrontMotor = null;
+    private DcMotor rightFrontMotor = null;
+    private double speed = 0.75;
+    //Change this value as necessary. This is the max speed the motors will go.
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftBackMotor  = hardwareMap.get(DcMotor.class, "LeftBackMotor");
+        leftFrontMotor  = hardwareMap.get(DcMotor.class, "LeftFrontMotor");
+        rightBackMotor  = hardwareMap.get(DcMotor.class, "RightBackMotor");
+        rightFrontMotor = hardwareMap.get(DcMotor.class, "RightFrontMotor");
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightBackMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+        telemetry.addData("HYPE", "ARE! YOU! READY?!?!?!?!");
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+
+        runtime.reset();
+        telemetry.addData("HYPE", "LEZ GOOOOOOOO!!!");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        //This refers to the functions below. Don't mess with them but do look at them.
+        singleJoystickDrive();
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", String.format("Back Left Power(%.2f)", leftBackMotor.getPower()));
+        telemetry.addData("Motors", String.format("Back Right Power(%.2f)", rightBackMotor.getPower()));
+        telemetry.addData("Motors", String.format("Front Left Power(%.2f)", leftFrontMotor.getPower()));
+        telemetry.addData("Motors", String.format("Front Right Power(%.2f)", rightFrontMotor.getPower()));
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        telemetry.addData("Status", "Robot Stopped");
+    }
+
+    /******************************************************************************
+
+     These three functions do all of the math and fun stuff to make the mecanum drive work.
+     There's only one thing you should have to touch ever.
+
+    *****************************************************************************/
+
+
+    public void setIndividualPowers ( float[] motorPowers){
+        // This function creates an array so that the function below works.
+
+        if (motorPowers.length != 4) {
+            return;
+        }
+        leftFrontMotor.setPower(-motorPowers[0]);
+        rightFrontMotor.setPower(-motorPowers[1]);
+        leftBackMotor.setPower(-motorPowers[2]);
+        rightBackMotor.setPower(-motorPowers[3]);
+        //If the controls seem inverted, remove these negative signs above as necessary
+    }
+
+    private void singleJoystickDrive () {
+        // We don't really know how this function works, but it makes the wheels drive, so we don't question it.
+        // Don't mess with this function unless you REALLY know what you're doing.
+
+        float rightX = -this.gamepad1.right_stick_x;
+        float leftY = this.gamepad1.left_stick_y;
+        float leftX = -this.gamepad1.left_stick_x;
+
+        float[] motorPowers = new float[4];
+
+            motorPowers[0] = (leftY + leftX + rightX);
+            motorPowers[1] = (leftY - leftX - rightX);
+            motorPowers[2] = (leftY - leftX + rightX);
+            motorPowers[3] = (leftY + leftX - rightX);
+
+
+        float max = getLargestAbsVal(motorPowers);
+        if (max < 1) {
+            max = 1;
+        }
+
+        for (int i = 0; i < motorPowers.length; i++) {
+            motorPowers[i] *= (speed / max);
+
+            float abs = Math.abs(motorPowers[i]);
+            if (abs < 0.05) {
+                motorPowers[i] = 0.0f;
+            }
+            if (abs > 1.0) {
+                motorPowers[i] /= abs;
+            }
+        }
+
+        setIndividualPowers(motorPowers);
+
+    }
+
+    private float getLargestAbsVal ( float[] values){
+        // This function does some math!
+        float max = 0;
+        for (float val : values) {
+            if (Math.abs(val) > max) {
+                max = Math.abs(val);
+            }
+        }
+        return max;
+    }
+
+}
