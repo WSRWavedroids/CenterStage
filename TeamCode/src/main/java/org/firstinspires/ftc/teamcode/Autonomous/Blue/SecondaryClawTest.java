e1e5b3bbaba908e4170bb5555cd95ab2bd8cea67
ClayGameDev <claytonkramer007@gmail.com>
2023-12-21 21:00:19 -0600

    Secondary Claw??????

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Blue/SecondaryClawTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.Autonomous.Blue;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import org.firstinspires.ftc.teamcode.Autonomous.AutonomousPLUS;
+
+@Disabled
+@Autonomous(group = "Stage", name = "Secondary Claw Test")
+    public class SecondaryClawTest extends AutonomousPLUS {
+        @Override
+        public void runOpMode() {
+
+            super.runOpMode();
+
+            //Do this to pass inspection.
+            waitForStart();
+            robot.closeSecondaryClaw();
+            prepareNextAction(800);
+            robot.openSecondaryClaw();
+            robot.closeSecondaryClaw();
+            prepareNextAction(800);
+            robot.openSecondaryClaw();
+            robot.closeSecondaryClaw();
+            prepareNextAction(800);
+            robot.openSecondaryClaw();
+            robot.closeSecondaryClaw();
+            prepareNextAction(800);
+            robot.openSecondaryClaw();
+
+            // code goes here
+
+
+        }
+    }
+
+
